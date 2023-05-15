932bfc38e9cec194b35cefd87d829f42245a190f
Nagraj <tadingi.nagraj.nagraj7@gmail.com>
2023-05-15 11:06:16 +0530

    badges

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,14 +2,22 @@ import star from "./images/star.png"
 import  PropTypes  from "prop-types"
 
 export default function Card(props){
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return(
         <div className="card">
-            <img src={props.img} alt="katie" className="card-image" />
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            
+            <img src={props.img} alt="image" className="card-image" />
             <div className="card-stats">
                 <img src={star} alt="star" className="card-star"/>
                 <span>{props.num}</span>
                 <span className="gray">(6)•</span>
-                <span className="gray">USA</span>
+                <span className="gray">{props.country}</span>
             </div>
             <p className="para">{props.line}</p>
             <p className="price"><span className="bold">From {props.price}</span>/person</p>
@@ -21,5 +29,8 @@ Card.propTypes={
     img: PropTypes.string,
     num: PropTypes.string,
     line: PropTypes.string,
-    price: PropTypes.string
+    price: PropTypes.string,
+    country: PropTypes.string,
+    openSpots: PropTypes.number,
+    location: PropTypes.string,
 }
---
+++ src/Carddata.jsx
@@ -0,0 +1,105 @@
+import Pic2  from './images/2.jpg'
+import Pic9 from './images/9.jpg'
+import Pic3  from './images/3.jpg'
+import Pic4  from './images/4.jpg'
+import Pic5  from './images/5.jpg'
+import Pic6  from './images/6.jpg'
+import Pic7  from './images/7.jpg'
+import Pic8  from './images/8.jpg'
+import Pic1 from "./images/katie-zaferes.png"
+import Pic10 from "./images/10.jpg"
+
+
+export default [
+    {
+        img:Pic1, 
+        price: "$142", 
+        line:"Life lessons from Kayie Zaferes", 
+        num:'5.0',
+        country:"USA",
+        openSpots:0,
+        location: "Online",
+    },
+    {
+        img:Pic2,
+        price: "$150", 
+        line:"Content writing from Gabby Sanders",
+        num:'4.6',
+        country: "Canada",
+        openSpots:27,
+        location: "Online",
+    },
+    {
+        img:Pic3, 
+        price: "$110", 
+        line:"Programming from John Doe", 
+        num:'4.8',
+        country: "Canada",
+        openSpots: 32,
+        // location: "Online",
+    },
+    {
+        img:Pic4, 
+        price: "$200", 
+        line:"HR lessons from Ria Matthews",
+        num:'5.2',
+        country: "Brazil",
+        openSpots:5,
+        location: "Online",
+    },
+    {
+        img:Pic5, 
+        price: "$160", 
+        line:"Body building from Tommy Zen",
+        num:'4.3',
+        country: "Mexico",
+        openSpots:4,
+        // location: "Online",
+    },
+    {
+        img:Pic6, 
+        price: "$130", 
+        line:"Social Media from Samantha Ruben",
+        num:'5.8',
+        country: "Indonesia",
+        openSpots:10,
+        // location: "Online",
+    },
+    {
+        img:Pic7, 
+        price: "$180" ,
+        line:"Fiction Writing from Peter Pan",
+        num:'5.5',
+        country: "Japan",
+        openSpots:12,
+        // location: "Online",
+    },
+    {
+        img:Pic8, 
+        price: "$220", 
+        line:"Yoga from Zadie Smith",
+        num:'5.1',
+        country: "Cuba",
+        openSpots:11,
+        // location: "Online",
+    },
+    {
+        img:Pic9, 
+        price: "$250", 
+        line:"Photography from Chris Lee",
+        num:"5.3",
+        country: "Austrilia",
+        openSpots:5,
+        // location: "Online",
+    },
+    {
+        img:Pic10,
+        price:"$160",
+        line: "Mental Health Coach",
+        num:"5.4",
+        country: "Nigeria",
+        openSpots:8,
+        // location: "Online",
+    }
+
+]
